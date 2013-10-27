553d9244081811e25cddfed4c811f1c13a6a97ef
Henry Stigler <stiglerh@gmail.com>
2013-10-26 22:29:52 -0400

    Updated profile page and added scoped status updated to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Treebook::Application.routes.draw do
+  get "profiles/show"
+
   devise_for :users
 
 devise_scope :user do
@@ -10,6 +12,8 @@ end
   resources :statuses
   get 'newsfeed', to: 'statuses#index', as: :newsfeed
   root to: 'statuses#index'
+
+  get '/:id', to: 'profiles#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
